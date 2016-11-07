b539834948eed36324f6f32d929fea228fe9856a
Max Strübing <max_struebing@gmx.de>
2016-11-07 14:13:00 +0100

    TASK: Add faster 008 solution

--- project-euler/008/haskell/008.hs
+++ project-euler/008/haskell/008.hs
@@ -1,2 +1,13 @@
 pythagoranTriplet :: Int -> Int
 pythagoranTriplet x = foldl (*) 1 $ head [[a, b, c] | a <- [1..x], b <- [1..x], c <- [1..x], a < b, b < c, a + b + c == 1000, a*a + b*b == c*c]
+
+
+-- faster solution I have coded some time ago
+run :: IO ()
+run = print $ (\(a,b,c) -> a*b*c) special
+
+special :: (Int, Int, Int)
+special = head $ filter (\(a,b,c) -> a*a+b*b == c*c) triples
+
+triples :: [(Int, Int, Int)]
+triples = [(h, k, 1000 - h - k) | h <- [1..500], k <- [1..h]]
