35999b331186b7ffe0c47972b39dff2e8ccc4382
Max Strübing <mxstrbng@gmail.com>
2018-12-04 14:01:07 +0100

    add aoc 2018/3/b

--- adventofcode/2018/3/app/Main.hs
+++ adventofcode/2018/3/app/Main.hs
@@ -1,11 +1,15 @@
 module Main where
 
 import Lib
+import Data.List
+import qualified Data.Map as Map  
 
 main :: IO ()
 main = do
     putStrLn "partOne:"
     partOne
+    putStrLn "partTwo:"
+    partTwo
 
 partOne :: IO ()
 partOne = do
@@ -15,5 +19,16 @@ partOne = do
     let count = length . filter (\i -> snd i > 1) $ frequency occupyingCoordinates
     print count
 
--- partTwo :: IO ()
--- partTwo = readFile ("input.txt") >>= print . . lines
+partTwo :: IO ()
+partTwo = do
+    input <- readFile "input.txt"
+    let inputLines = lines input
+
+    let board = constructBoard boardSize
+    let occupyingCoordinates = map getOccupyingCoordinates inputLines
+    let finalBoard = occupyAll' board $ concat occupyingCoordinates
+    let stuff = map (\coordsList -> map (\coord -> Map.lookup coord finalBoard) coordsList) occupyingCoordinates
+    let moreStuff = map (\s -> all (== Just 1) s) stuff
+    let abc = elemIndex True moreStuff
+    print abc
+
--- adventofcode/2018/3/src/Lib.hs
+++ adventofcode/2018/3/src/Lib.hs
@@ -13,34 +13,34 @@ module Lib
 
 import Data.Maybe
 import Data.List
-import Data.Map (fromListWith, toList)
+import qualified Data.Map as Map  
 
 type Coordinates = (Int, Int)
-type Board = [(Coordinates, Int)]
+type Board = Map.Map Coordinates Int
 
 boardSize :: Int
 boardSize = 1000
 
 constructBoard :: Int -> Board
-constructBoard size = [ ((x, y), 0) | x <- [0..size], y <- [0..size] ]
+constructBoard size = Map.fromList [((x, y), 0) | x <- [0..size], y <- [0..size]]
 
 occupyAll :: Board -> [Coordinates] -> Board
 occupyAll board (x:xs) 
-  | xs == [] = board
+  | xs == [] = occupy board x
   | otherwise = occupyAll (occupy board x) xs
 
 
 frequency :: (Ord a) => [a] -> [(a, Int)]
-frequency xs = toList (fromListWith (+) [(x, 1) | x <- xs])
+frequency xs = Map.toList (Map.fromListWith (+) [(x, 1) | x <- xs])
 
 occupyAll' :: Board -> [Coordinates] -> Board
-occupyAll' board coordsList = foldr (\curr acc -> occupy acc curr) board coordsList
+occupyAll' board coordsList = foldr (\c b -> Map.insertWith (+) c 1 b) board coordsList
 
 occupy :: Board -> Coordinates -> Board
-occupy board coords = map (\i -> if (fst i == coords) then (fst i, snd i + 1) else i) board
+occupy board coords = Map.fromList $ map (\i -> if (fst i == coords) then (fst i, snd i + 1) else i) $ Map.toList board
 
 countCoordsOccupiedMoreThanOnce :: Board -> Int
-countCoordsOccupiedMoreThanOnce = length . filter (\i -> snd i > 1) 
+countCoordsOccupiedMoreThanOnce = length . filter (\i -> snd i > 1) . Map.toList
 
 getOccupyingCoordinates :: String -> [(Coordinates)]
 getOccupyingCoordinates input = [(x, y) | x <- [startX .. startX + width -1], y <- [startY .. startY + height -1] ]
