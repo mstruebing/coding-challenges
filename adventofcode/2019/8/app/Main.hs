53866eef302687bd9264ab54c9d0004739a545c4
Max Strübing <mxstrbng@gmail.com>
2019-12-08 18:02:25 +0100

    Add day 8 part two

--- adventofcode/2019/8/app/Main.hs
+++ adventofcode/2019/8/app/Main.hs
@@ -2,11 +2,12 @@ module Main where
 
 import Data.List.Split (chunksOf)
 import Data.Char (isDigit, digitToInt)
-import Data.List (elemIndex)
+import Data.List (elemIndex, intercalate)
 import Data.Maybe (fromJust)
 
 type Image = [[Int]]
 type Layer = [Int]
+type Color = Int
 
 imageWidth :: Int
 imageWidth = 25
@@ -17,8 +18,49 @@ imageHeight = 6
 imageFile :: String
 imageFile = "input.txt"
 
+black :: Int
+black = 0
+
+white :: Int
+white = 1
+
+transparent :: Int
+transparent = 2
+
 main :: IO ()
-main = 
+main = do
+    part_one
+    part_two
+
+part_two :: IO ()
+part_two = 
+    putStrLn
+    . intercalate "\n"
+    . chunksOf imageWidth
+    . convertToAsci
+    . getMergedLayer
+    . parse 
+      =<< readFile imageFile 
+
+convertToAsci :: Layer -> [Char]
+convertToAsci = map(\x -> if x == black then ' ' else '#')
+
+getMergedLayer :: Image -> Layer
+getMergedLayer layers = 
+    foldl (\acc curr -> mergeLayer acc curr) 
+    (replicate (length $ layers !! 0) transparent) 
+    layers
+
+mergeLayer :: Layer -> Layer -> Layer
+mergeLayer layerOne layerTwo = 
+    map mergeColor $ zip layerOne layerTwo
+
+mergeColor :: (Color, Color) -> Color
+mergeColor x = 
+    if fst x == transparent then snd x else fst x
+
+part_one :: IO ()
+part_one = 
     print 
     . multiplyNumbersOfLayer 1 2
     . getLayerWithLeastNumbers 0 
