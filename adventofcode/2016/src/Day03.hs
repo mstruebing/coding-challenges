50ef45c8398a420f201b27bcf099a0017bbcde65
Max Strübing <mxstrbng@gmail.com>
2023-11-25 10:23:55 +0000

    2016/d3/p2

--- adventofcode/2016/src/Day03.hs
+++ adventofcode/2016/src/Day03.hs
@@ -1,6 +1,8 @@
-module Day03 (solve, part1, part2, possible, parseLine) where
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+module Day03 (solve, part1, part2, possible, parseLine, parseSecond, Triangle) where
 
 import Data.List (sort)
+import Data.List.Split (chunksOf)
 
 type Triangle = (Int, Int, Int)
 
@@ -11,11 +13,19 @@ part1 :: String -> String
 part1  = show . length . filter id .  map (possible . parseLine) . lines
 
 part2 :: String -> String
-part2 _ = "Not implemented"
-
+part2  = show . length . filter id . map possible . parseSecond . lines
 
 possible :: Triangle -> Bool
 possible (x,y,z) = x + y > z
 
 parseLine :: String -> Triangle
-parseLine  =  (\[x, y,z] -> (x,y,z)) . sort . map read . words 
+parseLine  =  (\[x, y,z] -> (x,y,z)) . sort . map read . words
+
+parseSecond :: [String] -> [Triangle]
+parseSecond =
+    map ((\[x, y, z] -> (x, y, z)) . sort) . 
+    chunksOf 3 .
+    concat .
+    foldl (\[first,second,third] (x,y,z) -> [first ++ [x], second ++ [y], third ++ [z]]) [[], [], []]  .
+    map ((\[x, y,z] -> (read x, read y, read z)) . words )
+
--- adventofcode/2016/test/Days/Day03Spec.hs
+++ adventofcode/2016/test/Days/Day03Spec.hs
@@ -1,7 +1,13 @@
 module Days.Day03Spec (spec) where
 
 import Test.Hspec
-import Day03 (possible, parseLine)
+import Day03 (possible, parseLine , parseSecond, Triangle)
+
+input :: [String]
+input = ["101 301 501" , "102 302 502" , "103 303 503", "201 401 601" , "202 402 602", "203 403 603"]
+
+expected :: [Triangle]
+expected = [(101, 102, 103), (201, 202, 203), (301, 302, 303), (401, 402, 403), (501, 502, 503), (601, 602, 603)]
 
 spec :: Spec
 spec = do
@@ -16,3 +22,6 @@ spec = do
         parseLine "5 10 25" `shouldBe` (5, 10, 25)
       it "sample unsorted" $
         parseLine "5 25 10 " `shouldBe` (5, 10, 25)
+    describe "parseSecond" $ do
+      it "should parse correctly" $
+        parseSecond input `shouldBe` expected
