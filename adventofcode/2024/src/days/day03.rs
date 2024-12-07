4d1cd358643063469a9c57e038ed08d8d1e716fa
Max Strübing <mxstrbng@gmail.com>
2024-12-07 15:43:32 +0000

    add 2024 d3 p1

--- adventofcode/2024/src/days/day03.rs
+++ adventofcode/2024/src/days/day03.rs
@@ -1,25 +1,45 @@
-use nom::IResult;
+use nom::bytes::complete::tag;
+use nom::character::complete::{anychar, char, digit1};
+use nom::multi::{many0, many_till};
+use nom::{IResult, Parser};
 
 use crate::days::Day;
 
 pub struct Day03;
 
 impl Day for Day03 {
-  type Input = String;
+    type Input = Vec<(i32, i32)>;
 
-  fn parse(_input: &str) -> IResult<&str, Self::Input> {
-    unimplemented!("parser")
-  }
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        parse(input)
+    }
 
-  type Output1 = String;
+    type Output1 = String;
 
-  fn part_1(_input: &Self::Input) -> Self::Output1 {
-    unimplemented!("part_1")
-  }
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        input
+            .into_iter()
+            .fold(0, |acc, (a, b)| acc + (*a * b))
+            .to_string()
+    }
 
-  type Output2 = String;
+    type Output2 = String;
 
-  fn part_2(_input: &Self::Input) -> Self::Output2 {
-    unimplemented!("part_2")
-  }
+    fn part_2(_input: &Self::Input) -> Self::Output2 {
+        unimplemented!("part_2")
+    }
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<(i32, i32)>> {
+    many0(many_till(anychar, pair_parser).map(|r| r.1))(input)
+}
+
+fn pair_parser(input: &str) -> IResult<&str, (i32, i32)> {
+    let (leftover, (a, b)) = nom::sequence::separated_pair(
+        nom::sequence::preceded(tag("mul("), digit1),
+        tag(","),
+        nom::sequence::terminated(digit1, char(')')),
+    )(input)?;
+
+    Ok((leftover, (a.parse().unwrap(), b.parse().unwrap())))
 }
